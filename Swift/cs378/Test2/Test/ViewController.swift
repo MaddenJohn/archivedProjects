0ec0f8a4fd69be8692d66ecabd58a647edf6f009
Jonathan Madden <jm76685@utexas.edu>
2019-06-16 14:44:30 -0500

    Fixed files

---
+++ Swift/cs378/Test2/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by John Madden on 2/27/17.
+//  Copyright © 2017 cs378. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
